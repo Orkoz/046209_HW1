c231f95d0a057fdbe024c95dffdd803f76fe3fb1
Orkoz <kozlovskyor@gmail.com>
2018-04-26 18:51:43 +0300

    moving to cpp

--- signal.cpp
+++ signal.cpp
@@ -1,4 +1,4 @@
-// signals.c
+// signals.cpp
 // contains signal handler funtions
 // contains the function/s that set the signal handlers
 
@@ -6,4 +6,12 @@
 /* Name: handler_cntlc
    Synopsis: handle the Control-C */
 #include "signals.h"
+
+// Ctrl-Z sigmal handler
+void catch_ctrl_z(int sig_num){
+	this.stop_job();
+}
+
+
+
 
